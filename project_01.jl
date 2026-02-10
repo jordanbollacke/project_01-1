32dfd02c9a525fa073519ce6ad73713da71fe9fb
Ryan C. Cooper <ryan.c.cooper@uconn.edu>
2026-02-10 16:02:37 -0500

    creating project_01 specs

--- project_01.jl
+++ project_01.jl
@@ -5,10 +5,28 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ f17103ea-06bf-11f1-a2b0-79e68ed152eb
-md"""![Spinning pendulum with rotating  and fixed coordinate systems.](./spinning_pendulum.png)"""
+md"""# Project_01 - Spinning Pendulum and the Lagrange equations
+
+In this project, a pendulum is attached to a spinning frame. The frame has dimensions, 
+
+$h_1 = 0.2~m$
+
+$w_1 = 0.1~m$
+
+and the pendulum length is $L=0.15~m$ with a $m=0.1~kg$ point mass at the end of the system. The pendulum swings in the $x'-z'-plane$ as it rotates at a constant speed, $\Omega$
+
+![Spinning pendulum with rotating  and fixed coordinate systems.](https://raw.githubusercontent.com/cooperrc/me5180-project_01/refs/heads/main/spinning_pendulum.svg)
+
+Your team's goal is to 
+
+- build the equations of motion using Lagrange and least action $L=T-V$
+- solve for the motion for a slow rotation speed and a fast rotation speed
+- visualize the solution with plots and animations
+
+"""
 
 # ╔═╡ 0d9be664-d7c5-4084-add2-25e5418742d6
-ls
+
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -27,7 +45,7 @@ project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
 """
 
 # ╔═╡ Cell order:
-# ╠═f17103ea-06bf-11f1-a2b0-79e68ed152eb
+# ╟─f17103ea-06bf-11f1-a2b0-79e68ed152eb
 # ╠═0d9be664-d7c5-4084-add2-25e5418742d6
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
